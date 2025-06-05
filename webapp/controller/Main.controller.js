03ca9d1b3f98561c625a4f65f0d4574631603a9a
Leslie Gonzales <leslie.gonzales-ext@vinci-construction.com>
2025-06-05 12:05:49 +0000

    Add sort

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,11 +1,22 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Sorter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/ui/model/FilterType"
+], (Controller, Sorter, Filter, FilterOperator, FilterType) => {
     "use strict";
 
     return Controller.extend("sapips.training.odata.controller.Main", {
-        onInit() {
-        },
-        
+            onAfterRendering: function () {
+                let oView = this.getView();
+                let oTable = oView.byId("oProdList");
+    
+                let oSort = new Sorter("ProductName", false);
+                oTable.getBinding("items").sort(oSort);
+    
+                let oFilter = new Filter("Discontinued", FilterOperator.EQ, false);
+                oTable.getBinding("items").filter([oFilter], sap.ui.model.FilterType.Control);
+        }
     });
 });
